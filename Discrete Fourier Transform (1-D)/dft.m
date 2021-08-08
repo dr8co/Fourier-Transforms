e952f41880257dabb18bcb41f41326f4e61eaf5f
Codes by Ian <76043277+dr8co@users.noreply.github.com>
2021-08-08 15:57:56 +0300

    Implementation in MATLAB

--- Discrete Fourier Transform (1-D)/dft.m
+++ Discrete Fourier Transform (1-D)/dft.m
@@ -1,21 +1,21 @@
 %This is an implementation of the Fourier Transform in MATLAB.
 %The results are similar to those obtained in the python implementation.
 
-signalLength = 0.5; %Duration in seconds
-sampleRate = 900; %in Hz
+signalLength = 0.5; % Duration in seconds
+sampleRate = 900; % in Hz
 
-dt = 1 / sampleRate; %time interval between two samples
-df = 1 / signalLength; %frequency between points
+dt = 1 / sampleRate; % time interval between two samples
+df = 1 / signalLength; % frequency between points
 
-t = 0:dt:signalLength; %time points
+t = 0:dt:signalLength; % time points
 [~, n_t] = size(t);
 
-%creating two sine waves:
+% creating two sine waves:
 sin_wave1 = sin(2*pi*50*t);
 sin_wave2 = sin(2*pi*70*t + pi/4);
 
-y = sin_wave1 + sin_wave2; %y as a function of the sum of the two waves
-f = fft(y); %compute Fourier Transform of y
+y = sin_wave1 + sin_wave2; % y as a function of the sum of the two waves
+f = fft(y); % compute Fourier Transform of y
 
 k = 0:2:(n_t);
 freq = df.*k;
@@ -29,7 +29,7 @@ title('Input data vs Time')
 xlabel('time (s)')
 ylabel('signal')
 
-%Plotting the frequency spectrum:
+% Plotting the frequency spectrum:
 subplot(2,1,2)
 plot(freq, p(1:n_f))
 title('frequency spectrum')
